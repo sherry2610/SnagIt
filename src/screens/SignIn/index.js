2728b0d9e510cc521729c3fb510432db5aae8c5a
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-12-17 05:23:44 +0500

    feat:integrate signin endpoint

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,6 +1,9 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import React, {useState} from 'react';
 import { TouchableOpacity } from 'react-native';
 import emailIcon from '../../assets/editProfile/email.png';
+import api from '../../utils/apiUtils/api';
+import { validateEmail } from '../../utils/helperUtils/generalUtils';
 import {
   SignInWrapper,
   SignInContent,
@@ -19,6 +22,45 @@ import {
 const SignIn = ({navigation}) => {
   const [emailFocus, setEmailFocus] = useState(false);
   const [passwordFocus, setPasswordFocus] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+
+    if(!email || !password ){
+      alert("All fields are required")
+      return
+    }
+
+    if(!validateEmail(email)) return
+
+    const payload = {
+      username: email,
+      password,
+  }
+
+  api.signInApi(payload)
+  .then(data => {
+    console.log("res ------",data); // JSON data parsed by `data.json()` call
+    if(data.success){
+      setEmail('')
+      setPassword('')
+      console.log("user registered!!!",data.user)
+      AsyncStorage.setItem("userData",JSON.stringify(data.user))
+      navigation.navigate('HomeScreen')
+      alert("Login Successfull")
+    } else {
+      if(data.err){
+        alert(data.err.message)
+      }
+    }
+  })
+  .catch(err=>{
+    console.log("error in signup ------", err)
+    alert("Something went wrong")
+  })
+
+  }
 
   return (
     <Wrapper>
@@ -36,6 +78,8 @@ const SignIn = ({navigation}) => {
           {emailFocus && <Legend widthValue='40px' >Email</Legend>}
           <EmailInput
             placeholder="Email address"
+            value={email}
+            onChangeText={text=>setEmail(text)}
             onFocus={() => setEmailFocus(true)}
             onBlur={() => setEmailFocus(false)}
             isFocused={emailFocus}
@@ -47,6 +91,9 @@ const SignIn = ({navigation}) => {
           {passwordFocus && <Legend widthValue='70px' >Password</Legend>}
           <EmailInput
             placeholder="Password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text=>setPassword(text)}
             onFocus={() => setPasswordFocus(true)}
             onBlur={() => setPasswordFocus(false)}
             isFocused={passwordFocus}
@@ -54,7 +101,7 @@ const SignIn = ({navigation}) => {
           <InputIcon source={emailIcon} />
         </InputWrapper>
 
-        <TouchableOpacity onPress={()=>alert("sign in!")}>
+        <TouchableOpacity onPress={()=>handleSubmit()}>
         <SaveButton>GET SNAG'N</SaveButton>
         </TouchableOpacity>
         <LastRow>
